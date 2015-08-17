f56238f702c354b8d23d9a07eaf86561b3423b9a
Patrick Stearns <pdeans1986@gmail.com>
2015-08-17 09:32:16 -0700

    Fix to conditional logic and trim original description before splitting.

--- product-description-read-more-less.js
+++ product-description-read-more-less.js
@@ -6,15 +6,15 @@
 --------------------------------------------------------------------------------*/
 function readMoreLess($el) {
 	var origDesc  = $el.text();  // Cache the full description text
-	var shortDesc = origDesc.split(/\s+/g);  // Strip out all white space and store the words of the description
+	var shortDesc = origDesc.trim().split(/\s+/g);  // Strip out all white space and store the words of the description
 	var cutOff    = 47;  // Number of words to show before cut off point
 
 	// If number of words in the description are greater than the cut off point...
-	if ( (shortDesc.length + 1) > cutOff ) {
+	if ( shortDesc > cutOff ) {
 		// Create the shortened product description by slicing off all words from the
 		// original description up to the cut off point. Add a space between each word and
 		// add '...' to the end of the shortened description.
-		shortDesc = shortDesc.slice(0, (cutOff + 1)).join(' ') + '...';
+		shortDesc = shortDesc.slice(0, cutOff).join(' ') + '...';
 		// Re-populate the element's html with the shortened product description
 		// and a 'Read More' link.
 		$el.html(shortDesc + '<a href="#" class="read-more">Read More</a>');
