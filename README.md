a199463b8aa0ff979fa7f0fa8fc8e131b9c08483
Patrick Stearns <pdeans1986@gmail.com>
2015-08-28 17:09:01 -0700

    Several improvements to function structure and logic. Updated README doc.

--- README.md
+++ README.md
@@ -1,22 +1,33 @@
 # Product Description - Read More/Read Less
 
-Simple js function to hide/show the full description for a product.
+Simple js function to hide/show the full or shortened description for a product.
 
 **How To Use:**<br>
-Copy and paste in the `readMoreLess` function code, and run it in any type of ready function.
+Copy and paste in the `readMoreLess` function code to a script that will be run on the page.
 
 Ex: Calling `readMoreLess` on the `.product-information--description .small` class selector
 ```javascript
 $(document).ready(function() {
-	readMoreLess($('.product-information--description .small'));
+	readMoreLess($('.product-information--description .small'), 50);
 });
-
-...
-function readMoreLess($el) {
-	// code content
-}
 ```
 
 **Notes About the Function**<br>
 The functionality is fairly straight-forward. The comments in the function should be able to explain
-what is going on if customization/expansion is needed.
+what is going on if customization/expansion is needed. Basically, the first parameter should be a
+jQuery object and the second parameter should be the number of words to allow before the product
+description cut-off. The function will then hide/show the shortened or full product description
+based on the user's interaction with the 'Read More' and 'Read Less' links.
+
+The function is also made to play nicely with CSS transitions if desired when the shortened and full
+product descriptions are show/hidden. The CSS selector to target for transitions is '.mva-read-more-desc'.
+
+Ex:
+```css
+.mva-read-more-desc {
+	-webkit-transition: opacity 0.3s linear;
+	transition: opacity 0.3s linear;
+}
+```
+Note that opacity or font-size can be used for the transition-property, however, I find that opacity
+flows much smoother.
--- product-description-read-more-less.js
+++ product-description-read-more-less.js
@@ -5,12 +5,14 @@
 * @param  $el, jQuery selector - The element containing the product description
 --------------------------------------------------------------------------------*/
 function readMoreLess($el) {
-	var origDesc  = $el.text();  // Cache the full description text
-	var shortDesc = origDesc.trim().split(/\s+/g);  // Strip out all white space and store the words of the description
-	var cutOff    = 47;  // Number of words to show before cut off point
+	var origDesc      = $el.text();  // Cache the full description text
+	var shortDesc     = origDesc.trim().split(/\s+/g);  // Strip out all white space and store the words of the description
+	var cutOff        = 50;  // Number of words to show before cut off point
+	var shortDescShow = '';
+	var shortDescHide = '';
 
 	// If number of words in the description are greater than the cut off point...
-	if ( shortDesc > cutOff ) {
+	if ( shortDesc.length > cutOff ) {
 		// Create the shortened product description by slicing off all words from the
 		// original description up to the cut off point. Add a space between each word and
 		// add '...' to the end of the shortened description.
@@ -37,3 +39,65 @@ function readMoreLess($el) {
 	});
 }
 readMoreLess($('.product-information--description .small'));
+
+/*------------------------------------------------------------------------------
+* Product Description Read More/Less
+*   Hide/Show the full description for a product
+*
+* @param $el, jQuery selector - The element containing the product description
+* @param maxShow, int - The number of words to show before cut off point
+--------------------------------------------------------------------------------*/
+function readMoreLess($el, maxShow) {
+			var origDesc      = $el.text();  // Cache the full description text
+			var shortDesc     = origDesc.trim().split(/\s+/g);  // Strip out all white space and store the words of the description
+			var cutOff        = maxShow;
+			var shortDescShow = '';
+			// Build the span elements for the Read More/Read Less functionality
+			var $spanReadMore = $('<span>... <a href="#" class="mva-read-more">Read More</a></span>');
+			var $spanReadLess = $('<span style="display:none"><br><a href="#" class="mva-read-less">Read Less</a></span>');
+			var $spanDescHide = $('<span class="mva-read-more-desc" style="opacity:0; max-height:0; font-size:0"></span>');
+
+			// If number of words in the description are greater than the cut off point...
+			if (shortDesc.length > cutOff ) {
+				// Create the shortened product description by slicing off all words from the
+				// original description up to the cut off point.
+				shortDescShow = shortDesc.slice(0, cutOff).join(' ');
+				// Add a space before the hidden text
+				$spanDescHide.text(' ' + shortDesc.slice(cutOff).join(' '));
+				// Edit the element's text to only show the short description and 'Read More' link
+				// Also add the hidden description and 'Read Less' link to the element
+				$el.text(shortDescShow)
+				   .append($spanReadMore)
+				   .append($spanDescHide)
+				   .append($spanReadLess);
+			}
+			// 'Read More' link was clicked...
+			$el.on('click', '.mva-read-more', function() {
+				// Hide the 'Read More' link and show the full product description
+				$spanReadMore.hide();
+				$spanDescHide.css({
+					'opacity'   : '1',
+					'max-height': '999rem',
+					'font-size' : 'inherit'
+				})
+				// Show the 'Read Less' link
+				$spanReadLess.show();
+
+				return false;
+			});
+			// 'Read Less' link was clicked...
+			$el.on('click', '.mva-read-less', function() {
+				// Hide the 'Read Less' link and only show the shortened product description
+				$spanReadLess.hide();
+				$spanDescHide.css({
+					'opacity'   :'0',
+					'max-height':'0',
+					'font-size' :'0'
+				});
+				// Show the 'Read More' link
+				$spanReadMore.show();
+
+				return false;
+			});
+		}
+		readMoreLess($('.product-information--description .small'), 50);
